947aa97b39d5f00bf2e12b1221f26bf5e13076d0
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-13 12:50:19 +0530

    Bugfixes in dbconfig

--- braincabin/dbconfig.py
+++ braincabin/dbconfig.py
@@ -1,33 +1,33 @@
 from pathlib import Path
-from os.path import join
+import os
 
 try:
     DATABASE_ENGINE = os.environ['DATABASE_ENGINE']
-except:
+except KeyError:
     DATABASE_ENGINE = 'django.db.backends.sqlite3'
 
 try:
     DATABASE_NAME = os.environ['DATABASE_NAME']
-except:
-    DATABASE_NAME = join(Path(__file__).resolve().parent.parent, 'sqlite3')
+except KeyError:
+    DATABASE_NAME = os.path.join(Path(__file__).resolve().parent.parent, 'sqlite3')
 
 try:
     DATABASE_USER = os.environ['DATABASE_USER']
-except:
+except KeyError:
     DATABASE_USER = ''
 
 try:
     DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-except:
+except KeyError:
     DATABASE_PASSWORD = ''
 
 try:
     DATABASE_HOST = os.environ['DATABASE_HOST']
-except:
+except KeyError:
     DATABASE_HOST = ''
 
 try:
     DATABASE_PORT = os.environ['DATABASE_PORT']
-except:
+except KeyError:
     DATABASE_PORT = ''
 
